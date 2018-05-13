e52521cbee3cd2b1be6c791eed5b78e9b0c18bf8
arsolga <arsolga@localhost.localdomain>
2018-05-13 22:25:29 +0300

    subprocess initial call

--- py.py
+++ py.py
@@ -1,1 +1,5 @@
-subprocess.call(["ps aux > ~/Programms/pyDataMirroring/output.txt"])
+import subprocess
+command = "ps -A|grep 'chrome'"
+process_list = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+output = process_list.communicate()[0]
+print(output)

0b20db63428a79088d99d8a2abce180c29045891
arsolga <arsolga@localhost.localdomain>
2018-05-13 23:16:02 +0300

    cycle for variables added

--- py.py
+++ py.py
@@ -1,7 +1,11 @@
 import subprocess
-command = "ps -A|grep 'chrome'"
-process_list = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-output = process_list.communicate()[0]
-print(output)
+#command = "ps -A|grep 'chrome'"
+list_name = ["nautilus", "chrome", "firefox", "bash", "gnome-system-monitor", "pulseaudio", "cat", "sublime_text"]
+for i in list_name:
+    ps_name = i
+    command = "ps -A|grep {}".format(ps_name)
+    process_list = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    output = process_list.communicate()[0]
+    print(output)
 
 #comment

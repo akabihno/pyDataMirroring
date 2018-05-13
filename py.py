397dbd8ebda8d9c0977b7cb73d52340feab473e5
arsolga <arsolga@localhost.localdomain>
2018-05-13 22:29:12 +0300

    comment added

--- py.py
+++ py.py
@@ -3,3 +3,5 @@ command = "ps -A|grep 'chrome'"
 process_list = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
 output = process_list.communicate()[0]
 print(output)
+
+#comment

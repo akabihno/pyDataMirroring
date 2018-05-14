868ae08fa89022673f9ccb2a646c0d142ef119a5
arsolga <arsolga@localhost.localdomain>
2018-05-14 13:03:11 +0300

    tkinter transparency added

--- tkt.py
+++ tkt.py
@@ -1,13 +1,16 @@
 #tkinter test
+#import tkinter as Tk
 from tkinter import *
 root = Tk()
 root.title("Drag window here")
+root.wait_visibility(root)
+root.attributes("-alpha", 0.5)
 def url_open(event):
     call(["ls", "-l"])
 
 btn = Button(root,
              text="Click me",
-             width=30,height=5,
+             width=100,height=30,
              bg="white",fg="black")
 btn.bind("<Button-1>", url_open)
 btn.pack()
